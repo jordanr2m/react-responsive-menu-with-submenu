7a61ce963bee6bb8a50a358bee1b6ffa20749bbb
Jordan J <jordanr2m@gmail.com>
2024-06-20 14:37:48 -0700

    Initial commit.

---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <div>
+            <section className='welcome --center-all'>
+                <h1 className='--text-light'>Welcome to Lucy.com</h1>
+            </section>
+        </div>
+    )
+}
+
+export default Header
